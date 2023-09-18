68ac73a8e2689652c3c0fceb0fab4c1d9c42214e
om2123 <omvakhare150@gmail.com>
2023-09-18 07:16:38 +0530

    animated sba section

--- src/app/comp/Collections/ShopByAnime.tsx
+++ src/app/comp/Collections/ShopByAnime.tsx
@@ -20,45 +20,45 @@ export default function ShopByAnime() {
       <div className="text-center w-full text-4xl my-3"> Shop By Anime</div>
       <div className="grid md:grid-rows-2 ">
         <div className="flex  mt-3 shrink">
-            <div className="text-center p-1 ml-3">
+            <div className="text-center p-1 ml-3 hover:scale-110 ease-in transform transition-transform duration-300">
               <Image src={urls.deathNote} alt="death note" width={170} className="m-2" height={100} />
               <h3 className="font-semibold">Death Note</h3>
             </div>
-            <div className="text-center p-2 ml-3">
+            <div className="text-center p-2 ml-3  hover:scale-110 ease-in transform transition-transform duration-300">
               <Image src={urls.naruto} alt="death note" width={170} className="m-2" height={100} />
               <h3 className="font-semibold">Naruto</h3>
             </div>
-            <div className="text-center p-2 ml-3">
+            <div className="text-center p-2 ml-3  hover:scale-110 ease-in transform transition-transform duration-300">
               <Image src={urls.dragonBallZ} alt="death note" width={170} className="m-2" height={100} />
               <h3 className="font-semibold">Dragon Ball Z</h3>
             </div>
-            <div className="text-center p-2 ml-3 max-sm:hidden">
+            <div className="text-center p-2 ml-3 max-sm:hidden  hover:scale-110 ease-in transform transition-transform duration-300">
               <Image src={urls.chainSawMan} alt="death note" width={170} className="m-2" height={100} />
               <h3 className="font-semibold">Chain Saw Man</h3>
             </div>
-            <div className="text-center p-2 ml-3 max-sm:hidden">
+            <div className="text-center p-2 ml-3 max-sm:hidden  hover:scale-110 ease-in transform transition-transform duration-300">
               <Image src={urls.hunterXHunter} alt="death note" width={170} className="m-2" height={100} />
               <h3 className="font-semibold">Hunter X Hunter</h3>
             </div>
         </div>
         <div className="flex  mt-3">
-        <div className="text-center p-2 ml-3">
+        <div className="text-center p-2 ml-3  hover:scale-110 ease-in transform transition-transform duration-300">
               <Image src={urls.Mha} alt="death note" width={170} className="m-2" height={100} />
               <h3 className="font-semibold">My hero academia</h3>
             </div>
-        <div className="text-center p-2 ml-3">
+        <div className="text-center p-2 ml-3  hover:scale-110 ease-in transform transition-transform duration-300">
               <Image src={urls.aot} alt="death note" width={170} className="m-2" height={100} />
               <h3 className="font-semibold">Attack on tian</h3>
             </div>
-        <div className="text-center p-2 ml-3">
+        <div className="text-center p-2 ml-3  hover:scale-110 ease-in transform transition-transform duration-300">
               <Image src={urls.jojo} alt="death note" width={170} className="m-2" height={100} />
               <h3 className="font-semibold">Jojo&apos;s bizarre adventure</h3>
             </div>
-        <div className="text-center p-2 ml-3 max-sm:hidden">
+        <div className="text-center p-2 ml-3  hover:scale-110 ease-in transform transition-transform duration-300 max-sm:hidden">
               <Image src={urls.haikyu} alt="death note" width={170} className="m-2" height={100} />
               <h3 className="font-semibold">Haikyuu</h3>
             </div>
-        <div className="text-center p-2 ml-3 max-sm:hidden">
+        <div className="text-center p-2 ml-3 max-sm:hidden  hover:scale-110 ease-in transform transition-transform duration-300">
               <Image src={urls.blackCover} alt="death note" width={170} className="m-2" height={100} />
               <h3 className="font-semibold">Black Cover</h3>
             </div>
